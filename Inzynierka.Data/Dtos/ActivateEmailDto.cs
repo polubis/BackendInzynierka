db663c44c78a0830e87cec6fe7b0a450d4ef6a5b
polubis <polubis@wp.pl>
2018-07-19 18:41:29 +0100

    Changing DTO

--- Inzynierka.Data/Dtos/ActivateEmailDto.cs
+++ Inzynierka.Data/Dtos/ActivateEmailDto.cs
@@ -7,6 +7,9 @@ namespace Inzynierka.Data.Dtos
     public class ActivateEmailDto:BaseDto
     {
         public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+
         public DateTime ModifiedDate { get; set; }
     }
 }
